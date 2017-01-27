bdd5c4cb2a6b86f1f836d611194cabef36f3aa77
Andrew Ewing <drew@phenocode.com>
2017-01-27 09:46:02 -0700

    Updated support
    Added support for Mason 0.1.2

--- mason.config.js
+++ mason.config.js
@@ -1,8 +1,19 @@
-module.exports = {
-	plugins: ['mason.plugin.scaffold'],
-	scaffold: {
-		templates: {
-			'class': './templates/class.js'
-		}
+module.exports = (Mason) => {
+	if(Mason.compareVersion('0.1.1') !== -1) {
+		Mason.registerCommand('test', (input, conf, Mason, resolve, reject) => {
+			console.info('Test!');
+			resolve();
+		});
+	} else {
+		console.info('Mason version does not support inline runners.');
 	}
+
+	return {
+		plugins: ['mason.plugin.scaffold'],
+		scaffold: {
+			templates: {
+				'class': './templates/class.js'
+			}
+		}
+	};
 };
